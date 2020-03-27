f9d1cb3ce05d7c8b3e2095acb41010b8a203affb
Jannik Sinz <j.sinz@gmx.de>
2020-03-27 16:12:32 +0100

    Initial Commit

---
+++ Data Structures and Algorithms/src/ScoreBoard_FixesList/ScoreBoard.java
@@ -0,0 +1,33 @@
+package ScoreBoard_FixesList;
+import java.util.*;
+
+private class ScoreBoardEntry{
+	String playerName;
+	int score;
+
+	//Constructor
+	public ScoreBoardEntry() {
+		//TODO
+	}
+}
+
+public class ScoreBoard{ //Fixed Size Array
+	
+	private int numPlayers = 0;
+	//Declare Array
+	private ScoreBoardEntry[] board;
+	
+	//Constructor
+	public ScoreBoard(int arraySize) {
+		//initialize Array
+		board = new ScoreBoardEntry[arraySize];
+	}
+	
+	//Interface Functions
+	int size();
+	boolean isEmpty();
+	
+	void addElement(int index, ScoreBoardEntry element);
+	ScoreBoardEntry getElement(int index);
+	
+}
---
+++ Data Structures and Algorithms/src/ScoreBoard_FixesList/TestScoreBoard.java
@@ -0,0 +1,19 @@
+package ScoreBoard_FixesList;
+
+public class TestScoreBoard {
+
+	public static void main(String[] args) {
+		//create Array, Size 10
+		ScoreBoard myBoard = new ScoreBoard(10);
+		
+		//add new players in the Scoreboard
+		ScoreBoardEntry player1 = new ScoreBoardEntry("Ines", 40);
+		ScoreBoardEntry player1 = new ScoreBoardEntry("Giovanni", 43);
+		
+		myBoard.addElement(0, player1);
+		
+		//Display
+		ScoreBoardEntry
+	}
+
+}
